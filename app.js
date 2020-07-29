d3d23e797f3f22537b65840f4b23259413e9a4b4
Piotr Pacewicz <piotrp92@gmail.com>
2020-07-29 17:10:52 +0100

    make 2 files html and js

---
+++ app.js
@@ -0,0 +1,5 @@
+const input = document.querySelector(".number");
+const button = document.querySelector(".btn");
+const answer = document.querySelector(".answer");
+
+console.log(input, button, answer);
